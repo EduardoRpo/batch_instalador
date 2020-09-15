310884db5f44c5e99245663c8a34ea4e7440aa90
Teenus <55334171+svelandi@users.noreply.github.com>
2020-09-15 08:28:40 -0500

    conversion mysqli a PDO acceso batch record login

--- html/php/login.php
+++ html/php/login.php
@@ -9,18 +9,17 @@ if (!empty($_SESSION['active'])) {
             $alert = "Ingrese su usuario y password";
         } else {
             require_once('./conexion.php');
-            $usuario = mysqli_real_escape_string($conn, $_POST['usuario']);
-            $pass = md5(mysqli_real_escape_string($conn, $_POST['clave']));
+            
+            $usuario = $_POST['usuario'];
+            $pass = md5($_POST['clave']);
 
-            $query = mysqli_query($conn, "SELECT * 
-                                          FROM usuario, modulo 
-                                          WHERE user ='$usuario' AND clave='$pass' AND modulo.id=usuario.id_modulo");
+            $sql = "SELECT * FROM usuario, modulo WHERE user = :usuario AND clave=:pass AND modulo.id=usuario.id_modulo";
+            $query = $conn->prepare($sql);
+            $query->execute(['usuario' => $usuario,'pass' => $pass]);
+            $rows = $query->rowCount();
 
-            $result = mysqli_num_rows($query);
-            mysqli_close($conn);
-
-            if ($result > 0) {
-                $data = mysqli_fetch_array($query);
+            if ($rows > 0) {
+                $data = $query->fetch(PDO::FETCH_ASSOC);
                 $_SESSION['active'] = true;
                 $_SESSION['idUser'] = $data['id'];
                 $_SESSION['nombre'] = $data['nombre'];
@@ -30,17 +29,20 @@ if (!empty($_SESSION['active'])) {
                 $_SESSION['cargo'] = $data['id_cargo'];
                 $_SESSION['modulo'] = $data['modulo'];
                 $modulo = $data['modulo'];
-                
+
                 if ($data['id_modulo'] == 1) {
                     header('location: html/batch.php');
                 } else {
                     header("location: {$modulo}");
                 }
             } else {
-                //$alert = "El usuario o la contraseña no son validos";
+                $alert = "El usuario o la contraseña no son validos";
                 echo '<script>alertify.set("notifier","position", "top-right"); alertify.error("El usuario o contraseña no son validos.");</script>';
                 session_destroy();
             }
+
+
         }
+        
     }
 }
