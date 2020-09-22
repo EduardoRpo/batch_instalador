4934d3a0bdfc4654fd54ab3055af11aa9bacd87a
Teenus <55334171+svelandi@users.noreply.github.com>
2020-09-22 18:54:29 -0500

    Guardar condiciones del medio PDO, ampliacion de tamaño ventana condiciones del medio

--- admin/sistema/modulos.php
+++ admin/sistema/modulos.php
@@ -50,7 +50,7 @@
       <div class="panel-header panel-header-sm"></div>
       <div class="content">
         <div class="row">
-          <div class="col-md-6">
+          <div class="col-md-10">
             <div class="card">
               <div class="card-header">
                 <h4 class="card-title">Procesos Registrados</h4>
--- html/php/condicionesmedio.php
+++ html/php/condicionesmedio.php
@@ -1,71 +1,44 @@
 <?php
-  require_once('../../conexion2.php');
-
-  function utf8ize($d) {
-    if (is_array($d)) 
-        foreach ($d as $k => $v) 
-            $d[$k] = utf8ize($v);
-
-     else if(is_object($d))
-        foreach ($d as $k => $v) 
-            $d->$k = utf8ize($v);
-
-     else 
-        return utf8_encode($d);
-
-    return $d;
-    }
+if (!empty($_POST)) {
+  require_once('../../conexion.php');
+  require_once('../../admin/sistema/php/crud.php');
 
 
-$op = $_POST['operacion'];
+  $op = $_POST['operacion'];
 
-switch($op){
-  case 1: // obtener el tiempo para mostrar modal
-    $id_modulo = intval($_POST['modulo']);
-    
-    $query_tiempo = mysqli_query($conn, "SELECT t_min, t_max 
-                                         FROM condicionesmedio_tiempo 
-                                         WHERE id_modulo = $id_modulo");
-    
-    $result = mysqli_num_rows($query_tiempo);
-    
-    if($result > 0){
-    $data = mysqli_fetch_assoc($query_tiempo);
-    echo json_encode($data, JSON_UNESCAPED_UNICODE);
-    }
-    else{
-    echo json_encode('');
-    }
-    
-    mysqli_free_result($query_tiempo);
-    mysqli_close($conn);
-  
-  break;
+  switch ($op) {
+    case 1: // obtener el tiempo para mostrar modal
+      $id_modulo = intval($_POST['modulo']);
 
-  case 2: // guardar Condiciones del Medio
-    $nombre_modulo = $_POST['modulo'];
-    $temperatura = $_POST['temperatura'];
-    $humedad =  $_POST['humedad'];
-    $id_batch =  $_POST['id'];
+      $query = "SELECT t_min, t_max 
+              FROM condicionesmedio_tiempo 
+              WHERE id_modulo = $id_modulo";
 
-    $query_condicioneMedio = "INSERT INTO condicionesmedio (fecha, temperatura, humedad, id_batch, id_modulo) 
-                              VALUES( NOW() , '$temperatura', '$humedad', '$id_batch', (SELECT modulo.id FROM modulo WHERE modulo.modulo = '$nombre_modulo') )";
+      ejecutarQuerySelect($conn, $query);
+      break;
 
-    $result = mysqli_query($conn, $query_condicioneMedio);
-    
-    if(!$result){
-        die('Error');
-        echo 'No guardado. Error: '.mysqli_error($conn);
-      }else{
-        echo 'Almacenado';
-      } 
-    
-    mysqli_free_result($query_tiempo);
-    mysqli_close($conn);
-  break;
+    case 2: // guardar Condiciones del Medio
+      if (!empty($_POST)) {
+        $nombre_modulo = $_POST['modulo'];
+        $temperatura = $_POST['temperatura'];
+        $humedad =  $_POST['humedad'];
+        $id_batch =  $_POST['id'];
 
+        $sql = "INSERT INTO condicionesmedio (fecha, temperatura, humedad, id_batch, id_modulo) 
+                  VALUES( NOW() , :temperatura, :humedad, :id_batch, (SELECT modulo.id FROM modulo WHERE modulo.modulo = :nombre_modulo) )";
+        $query = $conn->prepare($sql);
+        $result = $query->execute([
+          'temperatura' => $temperatura,
+          'humedad' => $humedad,
+          'id_batch' => $id_batch,
+          'nombre_modulo' => $nombre_modulo,
+        ]);
+        if ($result) 
+          echo '1';
+        else
+          echo '2';
+
+      }
+      break;
+  }
 }
-
-    
-
-?>
