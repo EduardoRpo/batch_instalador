597e458e027d0af9ac885f8a411c4061eddd1903
Teenus <55334171+svelandi@users.noreply.github.com>
2020-09-22 12:51:06 -0500

    organizacion batch record iconos actualizar y eliminar

--- html/batch.php
+++ html/batch.php
@@ -96,8 +96,6 @@ include("modal/modal_cambiarContrasena.php");
               <thead>
                 <tr>
                   <th></th>
-                  <th></th>
-                  <th></th>
                   <th>No</th>
                   <th>Orden</th>
                   <th>Referencia</th>
@@ -109,7 +107,8 @@ include("modal/modal_cambiarContrasena.php");
                   <th>Programación</th>
                   <th>Estado</th>
                   <th>Multi</th>
-                  
+                  <th></th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
