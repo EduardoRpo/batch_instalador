5e7985f9323fa8d342680bcfbd0b346ef7a599f3
Teenus <55334171+svelandi@users.noreply.github.com>
2020-09-22 21:37:34 -0500

    fix instructivo

--- html/php/instructivo.php
+++ html/php/instructivo.php
@@ -4,7 +4,7 @@ require_once('../../admin/sistema/php/crud.php');
 
 //listar Condiciones del medio
 
-$query = "SELECT * FROM instructivo_preparacion WHERE id_producto = :referencia";
+$sql = "SELECT * FROM instructivo_preparacion WHERE id_producto = :referencia";
 $query = $conn->prepare($sql);
 $result = $query->execute(['referencia' => $referencia]);
 
