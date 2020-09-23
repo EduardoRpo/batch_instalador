341760a0f930150bdc8147d13ae4094ddfed17f6
Teenus <55334171+svelandi@users.noreply.github.com>
2020-09-22 21:59:49 -0500

    fix instructivo

--- html/php/instructivo.php
+++ html/php/instructivo.php
@@ -1,17 +1,19 @@
 <?php
-require_once('../../conexion.php');
-require_once('../../admin/sistema/php/crud.php');
+if (!empty($_POST)) {
+    require_once('../../conexion.php');
+    require_once('../../admin/sistema/php/crud.php');
 
-//listar Condiciones del medio
+    $referencia = $_POST['referencia'];
 
-$sql = "SELECT * FROM instructivo_preparacion WHERE id_producto = :referencia";
-$query = $conn->prepare($sql);
-$result = $query->execute(['referencia' => $referencia]);
+    $sql = "SELECT * FROM instructivo_preparacion WHERE id_producto = :referencia";
+    $query = $conn->prepare($sql);
+    $result = $query->execute(['referencia' => $referencia]);
 
-if ($result) {
-    while ($data = $query->fetch(PDO::FETCH_ASSOC)) {
-        $arreglo["data"][] = $data;
+    if ($result) {
+        while ($data = $query->fetch(PDO::FETCH_ASSOC)) {
+            $arreglo["data"][] = $data;
+        }
+
+        echo json_encode(utf8ize($arreglo), JSON_UNESCAPED_UNICODE);
     }
-
-    echo json_encode(utf8ize($arreglo), JSON_UNESCAPED_UNICODE);
 }
