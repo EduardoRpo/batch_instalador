7f17b03b8736e4288aeeb3fe0c2da3720cef75b6
Teenus <55334171+svelandi@users.noreply.github.com>
2020-09-23 00:18:10 -0500

    fix datos caracteristicas productos preparacion

--- api/src/dao/ProductDao.php
+++ api/src/dao/ProductDao.php
@@ -36,34 +36,28 @@
     public function findDetailsByProduct($idProduct)
     {
       $connection = Connection::getInstance()->getConnection();
-      $stmt = $connection->prepare("SELECT
-    producto.referencia,
-    producto.nombre_referencia,
-    color.color,
-    olor.olor,
-    apariencia.apariencia,
-    viscosidad.limite_inferior as limite_inferior_viscosidad ,
-    viscosidad.limite_superior as limite_superior_viscosidad,
-    densidad_gravedad.limite_inferior as limite_inferior_densidad_gravedad,
-    densidad_gravedad.limite_superior as limite_superior_densidad_gravedad,
-    untuosidad.untuosidad,
-    poder_espumoso.poder_espumoso,
-    grado_alcohol.limite_inferior as limite_inferior_grado_alcohol,
-    grado_alcohol.limite_superior as limite_superior_grado_alcohol,
-    ph.limite_inferior AS limite_inferior_ph,
-    ph.limite_superior AS limite_superior_ph
-FROM
-    producto
-LEFT JOIN olor ON olor.id = producto.id_color
-LEFT JOIN color ON color.id = producto.id_color
-LEFT JOIN apariencia ON apariencia.id = producto.id_apariencia
-LEFT JOIN ph ON ph.id = producto.id_ph
-LEFT JOIN viscosidad ON viscosidad.id = producto.id_viscosidad
-LEFT JOIN densidad_gravedad ON densidad_gravedad.id = producto.id_densidad_gravedad
-LEFT JOIN untuosidad ON untuosidad.id = producto.id_untuosidad
-LEFT JOIN poder_espumoso ON poder_espumoso.id = producto.id_poder_espumoso
-LEFT JOIN grado_alcohol ON grado_alcohol.id = producto.id_grado_alcohol
-WHERE producto.referencia = ?");
+      $stmt = $connection->prepare("SELECT producto.referencia, producto.nombre_referencia,
+      color.nombre, olor.nombre, apariencia.nombre,
+      viscosidad.limite_inferior as limite_inferior_viscosidad ,
+      viscosidad.limite_superior as limite_superior_viscosidad,
+      densidad_gravedad.limite_inferior as limite_inferior_densidad_gravedad,
+      densidad_gravedad.limite_superior as limite_superior_densidad_gravedad,
+      untuosidad.nombre, poder_espumoso.nombre,
+      grado_alcohol.limite_inferior as limite_inferior_grado_alcohol,
+      grado_alcohol.limite_superior as limite_superior_grado_alcohol,
+      ph.limite_inferior AS limite_inferior_ph,
+      ph.limite_superior AS limite_superior_ph
+      FROM producto
+      LEFT JOIN olor ON olor.id = producto.id_color
+      LEFT JOIN color ON color.id = producto.id_color
+      LEFT JOIN apariencia ON apariencia.id = producto.id_apariencia
+      LEFT JOIN ph ON ph.id = producto.id_ph
+      LEFT JOIN viscosidad ON viscosidad.id = producto.id_viscosidad
+      LEFT JOIN densidad_gravedad ON densidad_gravedad.id = producto.id_densidad_gravedad
+      LEFT JOIN untuosidad ON untuosidad.id = producto.id_untuosidad
+      LEFT JOIN poder_espumoso ON poder_espumoso.id = producto.id_poder_espumoso
+      LEFT JOIN grado_alcohol ON grado_alcohol.id = producto.id_grado_alcohol
+      WHERE producto.referencia = ?");
       $stmt->execute(array($idProduct));
       $products = $stmt->fetch($connection::FETCH_ASSOC);
       $this->logger->info(__FUNCTION__, array('query' => $stmt->queryString, 'errors' => $stmt->errorInfo()));
