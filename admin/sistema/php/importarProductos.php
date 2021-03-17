f5217849a64648bd5010ba50d98cb2b17b00dcd6
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-03-16 22:18:38 -0500

    fix datos insert productos

--- admin/sistema/php/importarProductos.php
+++ admin/sistema/php/importarProductos.php
@@ -106,14 +106,14 @@ if ($tabla == 'producto') {
 								:id_etiqueta, :id_empaque, :id_otros, :multi, :base_instructivo, :instructivo)";
 				$query = $conn->prepare($sql);
 				$result = $query->execute([
-					'producto' => $data[0], 'referencia' => $data[1], 'nombre_referencia' => $data[2], 'unidad_empaque' => $data[3],
-					'id_nombre_producto' => $data[4], 'id_notificacion_sanitaria' => $data[5], 'id_linea' => $data[6], 'id_marca' => $data[7],
-					'id_propietario' => $data[8], 'presentacion_comercial' => $data[9], 'id_color' => $data[10], 'id_olor' => $data[11],
-					'id_apariencia' => $data[12], 'id_untuosidad' => $data[13], 'id_poder_espumoso' => $data[14], 'id_recuento_mesofilos' => $data[15],
-					'id_pseudomona' => $data[16], 'id_escherichia' => $data[17], 'id_staphylococcus' => $data[18], 'id_ph' => $data[19],
-					'id_viscosidad' => $data[20], 'id_densidad_gravedad' => $data[21], 'id_grado_alcohol' => $data[22], 'id_tapa' => $data[23],
-					'id_envase' => $data[24], 'id_etiqueta' => $data[25], 'id_empaque' => $data[26], 'id_otros' => $data[27], 'multi' => $data[28],
-					'base_instructivo' => $data[29], 'instructivo' => $data[30]
+					'referencia' => $data[0], 'nombre_referencia' => $data[1], 'unidad_empaque' => $data[2],
+					'id_nombre_producto' => $data[3], 'id_notificacion_sanitaria' => $data[4], 'id_linea' => $data[5], 'id_marca' => $data[6],
+					'id_propietario' => $data[7], 'presentacion_comercial' => $data[8], 'id_color' => $data[9], 'id_olor' => $data[10],
+					'id_apariencia' => $data[11], 'id_untuosidad' => $data[12], 'id_poder_espumoso' => $data[13], 'id_recuento_mesofilos' => $data[14],
+					'id_pseudomona' => $data[15], 'id_escherichia' => $data[16], 'id_staphylococcus' => $data[17], 'id_ph' => $data[18],
+					'id_viscosidad' => $data[19], 'id_densidad_gravedad' => $data[20], 'id_grado_alcohol' => $data[21], 'id_tapa' => $data[22],
+					'id_envase' => $data[23], 'id_etiqueta' => $data[24], 'id_empaque' => $data[25], 'id_otros' => $data[26], 'multi' => $data[27],
+					'base_instructivo' => $data[28], 'instructivo' => $data[29]
 				]);
 			}
 		}
