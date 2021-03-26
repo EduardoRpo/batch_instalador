c2d24ef8a397b2c1923282e4a21f9b489f82123f
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-03-26 13:25:49 -0500

    cambio de estado acondicionamiento y envansado al mismo tiempo

--- api/src/dao/BatchLineaDao.php
+++ api/src/dao/BatchLineaDao.php
@@ -86,7 +86,7 @@ class BatchLineaDao
     $connection = Connection::getInstance()->getConnection();
     $stmt = $connection->prepare("SELECT batch.id_batch, batch.fecha_programacion, batch.numero_orden, batch.numero_orden, batch.id_producto as referencia, p.nombre_referencia, batch.numero_lote  
                                   FROM batch INNER JOIN producto p ON p.referencia = batch.id_producto 
-                                  WHERE (batch.estado >= 7) ORDER BY batch.id_batch DESC");
+                                  WHERE (batch.estado >= 6) ORDER BY batch.id_batch DESC");
     $stmt->execute();
     $this->logger->info(__FUNCTION__, array('query' => $stmt->queryString, 'errors' => $stmt->errorInfo()));
     $acondicionamiento = $stmt->fetchAll($connection::FETCH_ASSOC);
