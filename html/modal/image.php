a254cd86a6f3dd07a5ee7a6b89053c47a2ba00a8
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-04-16 11:23:20 -0500

    fix posicion modal imagen referencia

--- html/modal/image.php
+++ html/modal/image.php
@@ -1,7 +1,7 @@
 <div class="modal" id="imageRef" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content" style="width:46%">
-            <div class="modal-body">
+    <div class="modal-dialog modal-sm modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-body" style="display:flex; justify-content:center">
                 <img src="" id="img" class="img-rounded" alt="" width="200" height="236" />
             </div>
         </div>
