3bda6a276ef68324cb3e70c00d2f7312b85b5763
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-05-03 14:37:11 -0500

    fix ingreso rol 3 a todos los procesos

--- html/sesion/sesionbatch.php
+++ html/sesion/sesionbatch.php
@@ -3,7 +3,7 @@ session_start();
 
 if (empty($_SESSION['active'])) {
   header('location: ../');
-} else if ($_SESSION['rol'] != 1  && $_SESSION['rol'] != 2) {
+} else if ($_SESSION['rol'] != 1  && $_SESSION['rol'] != 2 && $_SESSION['rol'] != 3) {
   if ($_SESSION['idModulo'] != 1) {
     session_destroy();
     header('location: ../../');
