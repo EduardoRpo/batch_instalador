d1722ec948946f382912788591585dc1f7a7f4ae
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-05-24 22:45:30 -0500

    creacion cargue de batch fisicoquimico

--- api/src/dao/BatchLineaDao.php
+++ api/src/dao/BatchLineaDao.php
@@ -146,9 +146,11 @@ class BatchLineaDao
   public function findBatchFisicoquimica()
   {
     $connection = Connection::getInstance()->getConnection();
-    $stmt = $connection->prepare("SELECT batch.id_batch, batch.fecha_programacion, batch.numero_orden, batch.numero_orden, batch.id_producto as referencia, p.nombre_referencia, batch.numero_lote  
-                                  FROM batch INNER JOIN producto p ON p.referencia = batch.id_producto 
-                                  WHERE (batch.estado >= 5.5 AND batch.estado <= 7) ORDER BY batch.id_batch DESC");
+    $stmt = $connection->prepare("SELECT batch.id_batch, batch.fecha_programacion, batch.numero_orden, batch.numero_orden, batch.id_producto as referencia, p.nombre_referencia, batch.numero_lote 
+                                  FROM batch INNER JOIN producto p ON batch.id_producto = p.referencia 
+                                  WHERE batch.estado >= 5.5 AND batch.id_batch 
+                                  NOT IN (SELECT batch FROM `batch_firmas2seccion` WHERE modulo = 9) 
+                                  ORDER BY `batch`.`id_batch` ASC");
     $stmt->execute();
     $this->logger->info(__FUNCTION__, array('query' => $stmt->queryString, 'errors' => $stmt->errorInfo()));
     $fisicoquimica = $stmt->fetchAll($connection::FETCH_ASSOC);
