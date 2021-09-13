02e43667da9396379be50ec537d7a7ef9e0d7f3a
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-09-13 14:15:25 -0500

    actualizacion fn update firmas totales

--- html/php/fn/postInsertTotalFirmas.php
+++ html/php/fn/postInsertTotalFirmas.php
@@ -7,21 +7,25 @@ $query->execute();
 $batchs = $query->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($batchs as $batch) {
+    /* $batch = 478; */
     $sql = "SELECT * FROM batch_control_firmas WHERE batch = :batch";
     $query = $conn->prepare($sql);
     $query->execute(['batch' => $batch['id_batch']]);
+    /* $query->execute(['batch' => $batch]); */
     $batchFirmas = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    for ($i = 5; $i < 11; $i++) {
+    for ($i = 2; $i < 11; $i++) {
         $key = array_search($i, array_column($batchFirmas, 'modulo'));
 
-        if (!$key) {
+        if ($key) {
+            /* if (!$key) { */
 
             /* Validar si existe multipresentacion */
             $modulo = $i;
             $sql = "SELECT * FROM multipresentacion WHERE id_batch = :batch";
             $query_multi = $conn->prepare($sql);
             $query_multi->execute(['batch' => $batch['id_batch']]);
+            /* $query_multi->execute(['batch' => $batch]); */
             $rows_multi = $query_multi->rowCount();
 
             if ($modulo == 2) $total_firmas = 4;
@@ -35,10 +39,15 @@ foreach ($batchs as $batch) {
             if ($modulo == 10) $rows_multi == 0 ? $total_firmas = 3 : $total_firmas = $rows_multi;
 
 
-            $sql = "INSERT INTO batch_control_firmas (modulo, batch, cantidad_firmas, total_firmas) 
+            /* $sql = "INSERT INTO batch_control_firmas (modulo, batch, cantidad_firmas, total_firmas) 
                     VALUES(:modulo, :batch, :cantidad_firmas, :total_firmas)";
             $query_insert = $conn->prepare($sql);
-            $query_insert->execute(['modulo' => $i, 'batch' => $batch['id_batch'], 'cantidad_firmas' => 0, 'total_firmas' => $total_firmas]);
+            $query_insert->execute(['modulo' => $i, 'batch' => $batch['id_batch'], 'cantidad_firmas' => 0, 'total_firmas' => $total_firmas]); */
+
+            $sql = "UPDATE batch_control_firmas SET total_firmas = :total_firmas WHERE modulo = :modulo AND batch = :batch";
+            $query_update = $conn->prepare($sql);
+            $query_update->execute(['modulo' => $i, 'batch' => $batch['id_batch'], 'total_firmas' => $total_firmas]);
+            /* $query_update->execute(['modulo' => $i, 'batch' => $batch, 'total_firmas' => $total_firmas]); */
         }
     }
 }
