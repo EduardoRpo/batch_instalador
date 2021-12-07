3c8f21d415caadf106bb83d1031d50cf842176e9
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-12-06 22:20:28 -0500

    ajuste nombres de MP en etiquetas

--- html/js/global/etiquetas.js
+++ html/js/global/etiquetas.js
@@ -1,127 +1,127 @@
 let flagE = 0;
 
-$(document).ready(function () {
-  imprimirEtiquetas = (data) => {
-    tablePesaje.on("click", "tbody tr", function (e) {
-      e.preventDefault();
-      $(this).toggleClass("tr_hover ");
+$(document).ready(function() {
+    imprimirEtiquetas = (data) => {
+        tablePesaje.on("click", "tbody tr", function(e) {
+            e.preventDefault();
+            $(this).toggleClass("tr_hover ");
 
-      arrayData = [];
-      JSONData = {};
-      let codMateriaPrima = $(this).find("td:first").html();
-      let peso = $(this).find("td:nth-child(4)").html();
-      JSONData.orden = batch.numero_orden;
-      JSONData.referencia = codMateriaPrima;
-      JSONData.peso = peso;
-      arrayData.push(JSONData);
-      sessionStorage.setItem("batch", batch);
-      $("#imprimirEtiquetas").modal("show");
-    });
-  };
+            arrayData = [];
+            JSONData = {};
+            let codMateriaPrima = $(this).find("td:first").html();
+            let peso = $(this).find("td:nth-child(4)").html();
+            JSONData.orden = batch.numero_orden;
+            JSONData.referencia = codMateriaPrima;
+            JSONData.peso = peso;
+            arrayData.push(JSONData);
+            sessionStorage.setItem("batch", batch);
+            $("#imprimirEtiquetas").modal("show");
+        });
+    };
 });
 
-$("#btnimprimirEtiquetas").click(function (e) {
-  e.preventDefault();
-  exportarEtiquetas();
-  //imprimirEtiquetasVirtuales();
+$("#btnimprimirEtiquetas").click(function(e) {
+    e.preventDefault();
+    exportarEtiquetas();
+    //imprimirEtiquetasVirtuales();
 });
 
 const imprimirEtiquetasFull = (marmita) => {
-  const ref = batch.referencia;
-  $.ajax({
-    url: `../../api/materiasp/${ref}`,
-    success: function (materiaPrima) {
-      $.ajax({
-        url: `../../api/user/${modulo}/${idBatch}`,
-        success: function (usuario) {
-          /* if (usuario == "") {
-            alertify.set("notifier", "position", "top-right");
-            alertify.success(
-              "Finalice el proceso de despeje antes de continuar"
-            );
-            return false;
-          } */
-          modulo == 2
-            ? imprimirEtiquetasPesaje(materiaPrima, usuario)
-            : modulo == 3
-            ? imprimirEtiquetasPreparacion(marmita, usuario)
-            : imprimirEtiquetasAcondicionamiento(usuario);
+    const ref = batch.referencia;
+    $.ajax({
+        url: `../../api/materiasp/${ref}`,
+        success: function(materiaPrima) {
+            $.ajax({
+                url: `../../api/user/${modulo}/${idBatch}`,
+                success: function(usuario) {
+                    /* if (usuario == "") {
+                      alertify.set("notifier", "position", "top-right");
+                      alertify.success(
+                        "Finalice el proceso de despeje antes de continuar"
+                      );
+                      return false;
+                    } */
+                    modulo == 2 ?
+                        imprimirEtiquetasPesaje(materiaPrima, usuario) :
+                        modulo == 3 ?
+                        imprimirEtiquetasPreparacion(marmita, usuario) :
+                        imprimirEtiquetasAcondicionamiento(usuario);
+                },
+            });
         },
-      });
-    },
-  });
+    });
 };
 
 const imprimirEtiquetasPesaje = (materiaPrima, usuario) => {
-  operacion = 1;
-  arrayData = [];
-  for (let i = 0; i < materiaPrima.length; i++) {
-    pesaje = {};
-    pesaje.orden = batch.numero_orden;
-    pesaje.referencia = materiaPrima[i].referencia;
-    pesaje.peso =
-      ((materiaPrima[i].porcentaje / 100) * batch.tamano_lote) /
-      $("#Notanques").val();
-    pesaje.producto = batch.nombre_referencia;
-    pesaje.user = usuario.nombres;
-    arrayData.push(pesaje);
-  }
-  exportarEtiquetas(operacion, arrayData);
+    operacion = 1;
+    arrayData = [];
+    for (let i = 0; i < materiaPrima.length; i++) {
+        pesaje = {};
+        pesaje.orden = batch.numero_orden;
+        pesaje.referencia = materiaPrima[i].referencia;
+        pesaje.peso =
+            ((materiaPrima[i].porcentaje / 100) * batch.tamano_lote) /
+            $("#Notanques").val();
+        pesaje.producto = batch.nombre_referencia;
+        pesaje.user = usuario.nombres;
+        arrayData.push(pesaje);
+    }
+    exportarEtiquetas(operacion, arrayData);
 };
 
 const imprimirEtiquetasPreparacion = (marmita, usuario) => {
-  operacion = 2;
-  let preparacion = batch;
-  preparacion.tanque = marmita;
-  preparacion.usuario = usuario.nombres;
-  exportarEtiquetas(operacion, preparacion);
+    operacion = 2;
+    let preparacion = batch;
+    preparacion.tanque = marmita;
+    preparacion.usuario = usuario.nombres;
+    exportarEtiquetas(operacion, preparacion);
 };
 
 const imprimirEtiquetasAcondicionamiento = (usuario) => {
-  operacion = 3;
-  batch.usuario = usuario.nombres;
-  exportarEtiquetas(operacion, batch);
+    operacion = 3;
+    batch.usuario = usuario.nombres;
+    exportarEtiquetas(operacion, batch);
 };
 
 const imprimirEtiquetasRetencion = () => {
-  operacion = 4;
-  const referencia = batch.referencia;
-  $.ajax({
-    type: "POST",
-    url: "../../../html/php/etiquetas.php",
-    data: { idBatch, referencia },
-    success: function (response) {
-      $muestras_retencion = JSON.parse(response);
-      arrayData = [];
-      for (let i = 0; i < $muestras_retencion.length + 1; i++) {
-        retencion = {};
-        retencion.referencia = batch.referencia;
-        retencion.producto = batch.nombre_referencia;
-        retencion.presentacion = batch.presentacion;
-        retencion.lote = batch.numero_lote;
-        retencion.orden = batch.numero_orden;
-        if (i < $muestras_retencion.length)
-          retencion.consecutivo = $muestras_retencion[i]["muestra"];
-        else retencion.consecutivo = "Microbiología";
-        retencion.codigo = `Microbiología/${batch.numero_orden}/${batch.numero_lote}/${batch.referencia}`;
-        arrayData.push(retencion);
-      }
-      exportarEtiquetas(operacion, arrayData);
-    },
-  });
+    operacion = 4;
+    const referencia = batch.referencia;
+    $.ajax({
+        type: "POST",
+        url: "../../../html/php/etiquetas.php",
+        data: { idBatch, referencia },
+        success: function(response) {
+            $muestras_retencion = JSON.parse(response);
+            arrayData = [];
+            for (let i = 0; i < $muestras_retencion.length + 1; i++) {
+                retencion = {};
+                retencion.referencia = batch.referencia;
+                retencion.producto = batch.nombre_referencia;
+                retencion.presentacion = batch.presentacion;
+                retencion.lote = batch.numero_lote;
+                retencion.orden = batch.numero_orden;
+                if (i < $muestras_retencion.length)
+                    retencion.consecutivo = $muestras_retencion[i]["muestra"];
+                else retencion.consecutivo = "Microbiología";
+                retencion.codigo = `Microbiología/${batch.numero_orden}/${batch.numero_lote}/${batch.referencia}`;
+                arrayData.push(retencion);
+            }
+            exportarEtiquetas(operacion, arrayData);
+        },
+    });
 };
 
 const exportarEtiquetas = (operacion, arrayData) => {
-  $.ajax({
-    type: "POST",
-    url: "../../html/php/exportar.php",
-    data: { operacion, array: arrayData },
+    $.ajax({
+        type: "POST",
+        url: "../../html/php/exportar.php",
+        data: { operacion, array: arrayData },
 
-    success: function (response) {
-      alertify.set("notifier", "position", "top-right");
-      alertify.success("Datos para etiquetas exportados correctamente");
-    },
-  });
+        success: function(response) {
+            alertify.set("notifier", "position", "top-right");
+            alertify.success("Datos para etiquetas exportados correctamente");
+        },
+    });
 };
 
 /* Etiquetas virtuales */
@@ -134,9 +134,9 @@ const exportarEtiquetas = (operacion, arrayData) => {
   imprimirEtiquetasVirtuales();
 }); */
 
-$("#btnImprimirTodaslasEtiquetas").click(function (e) {
-  e.preventDefault();
-  imprimirEtiquetasFull();
+$("#btnImprimirTodaslasEtiquetas").click(function(e) {
+    e.preventDefault();
+    imprimirEtiquetasFull();
 });
 
 /* imprimirEtiquetasVirtuales = () => {
@@ -174,24 +174,24 @@ $("#btnImprimirTodaslasEtiquetas").click(function (e) {
 }; */
 
 const ImprimirEtiquetasInvima = () => {
-  $.ajax({
-    url: `../../api/etiquetasvirtualesinv/${referencia}/${idBatch}`,
-    success: function (response) {
-      if (!flagE) flagE = 1;
-      else return false;
+    $.ajax({
+        url: `../../api/etiquetasvirtualesinv/${referencia}/${idBatch}`,
+        success: function(response) {
+            if (!flagE) flagE = 1;
+            else return false;
 
-      for (let j = 0; j < response.length; j++) {
-        if (response[j]["fecha_registro"])
-          fecha = response[j]["fecha_registro"];
-        if (response[j]["urlfirma"]) verifico = response[j]["urlfirma"];
-        if (response[j]["cantidad"]) cantidad = response[j]["cantidad"];
-      }
+            for (let j = 0; j < response.length; j++) {
+                if (response[j]["fecha_registro"])
+                    fecha = response[j]["fecha_registro"];
+                if (response[j]["urlfirma"]) verifico = response[j]["urlfirma"];
+                if (response[j]["cantidad"]) cantidad = response[j]["cantidad"];
+            }
 
-      //$(".etiquetasV").empty();
+            //$(".etiquetasV").empty();
 
-      for (i = 0; i < response.length - 2; i++) {
-        $(".etiquetasV").append(
-          `<div class="etiquetaUnica rounded-3">
+            for (i = 0; i < response.length - 2; i++) {
+                $(".etiquetasV").append(
+                    `<div class="etiquetaUnica rounded-3">
             <div class="etiquetasVirtuales">
                 <p><b>OP: </b>${infoBatch.numero_orden}</p>
                 <p id="peso"><b>PESO: </b>${(
@@ -199,12 +199,13 @@ const ImprimirEtiquetasInvima = () => {
                   cantidad
                 ).toFixed(2)}</p>
                 <p><b>REFERENCIA:</b> ${response[i]["referencia"]}</p>
+                <p><b>NOMBRE MP:</b> ${response[i]["alias"]}</p>
                 <p><b>FECHA: </b> ${fecha}</p>
                 <p><b>VoBo QC: </b><img src="${verifico}" style="width:60%"></p>
             </div>
           </div>`
-        );
-      }
-    },
-  });
+                );
+            }
+        },
+    });
 };
