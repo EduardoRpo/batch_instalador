82071bc1db47e7fb32cb0202cc76ae8d5d6a0237
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-12-23 21:02:36 -0500

    creacion de nombre producto microbiologia

--- html/js/microbiologia/microbiologia.js
+++ html/js/microbiologia/microbiologia.js
@@ -1,61 +1,67 @@
-$(document).ready(function () {
-  $("#tblMicrobiogia").DataTable({
-    pageLength: 50,
-    order: [[0, "desc"]],
+$(document).ready(function() {
+    $("#tblMicrobiogia").DataTable({
+        pageLength: 50,
+        order: [
+            [0, "desc"]
+        ],
 
-    ajax: {
-      url: "/api/microbiologia",
-      dataSrc: "",
-    },
-    language: {
-      url: "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json",
-    },
-
-    columns: [
-      {
-        title: "Batch",
-        data: "id_batch",
-        className: "uniqueClassName",
-      },
-      {
-        title: "Fecha Programación",
-        data: "fecha_programacion",
-        className: "uniqueClassName",
-      },
-      {
-        title: "No Orden",
-        data: "numero_orden",
-        className: "uniqueClassName",
-      },
-      {
-        title: "Referencia",
-        data: "referencia",
-        className: "uniqueClassName",
-      },
-      {
-        title: "No Lote",
-        data: "numero_lote",
-        className: "uniqueClassName",
-      },
-      {
-        title: "Firmas G",
-        data: "cantidad_firmas",
-        className: "uniqueClassName",
-      },
-      {
-        title: "Firmas T",
-        data: "total_firmas",
-        className: "uniqueClassName",
-      },
-      {
-        title: "Ingresar",
-        data: "",
-        className: "uniqueClassName",
-        render: (data, type, row) => {
-          return `<a href="microbiologiainfo/${row.id_batch}/${row.referencia}" <i class="large material-icons" data-toggle="tooltip" title="Ingresar" style="color:rgb(0, 154, 68)">format_color_fill</i></a>`;
+        ajax: {
+            url: "/api/microbiologia",
+            dataSrc: "",
         },
-      },
-    ],
-  });
-  //table.destroy();
+        language: {
+            url: "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json",
+        },
+
+        columns: [{
+                title: "Batch",
+                data: "id_batch",
+                className: "uniqueClassName",
+            },
+            {
+                title: "Fecha Programación",
+                data: "fecha_programacion",
+                className: "uniqueClassName",
+            },
+            {
+                title: "No Orden",
+                data: "numero_orden",
+                className: "uniqueClassName",
+            },
+            {
+                title: "Referencia",
+                data: "referencia",
+                className: "uniqueClassName",
+            },
+            {
+                title: "Nombre Referencia",
+                data: "nombre_referencia",
+                className: "uniqueClassName",
+            },
+            {
+                title: "No Lote",
+                data: "numero_lote",
+                className: "uniqueClassName",
+            },
+            {
+                title: "Firmas G",
+                data: "cantidad_firmas",
+                className: "uniqueClassName",
+            },
+            {
+                title: "Firmas T",
+                data: "total_firmas",
+                className: "uniqueClassName",
+            },
+            {
+                title: "Ingresar",
+                data: "",
+                className: "uniqueClassName",
+                render: (data, type, row) => {
+                    return `<a href="microbiologiainfo/${row.id_batch}/${row.referencia}" <i class="large material-icons" data-toggle="tooltip" title="Ingresar" style="color:rgb(0, 154, 68)">format_color_fill</i></a>`;
+                },
+            },
+        ],
+    });
+    //table.destroy();
 });
