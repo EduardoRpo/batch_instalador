356e3cfd0b5dc508d254616861757423344432eb
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-02-08 08:56:23 -0500

    ajuste btn bloqueado

--- html/js/envasado/envasadoinfo.js
+++ html/js/envasado/envasadoinfo.js
@@ -137,7 +137,7 @@ function deshabilitarbotones() {
         $(`.controlpeso_verificado${i}`).prop("disabled", true);
         $(`.devolucion_realizado${i}`).prop("disabled", true);
         $(`.devolucion_verificado${i}`).prop("disabled", true);
-        $(`.btnEntregasParciales${i}`).prop("disabled", true);
+        //$(`.btnEntregasParciales${i}`).prop("disabled", true);
     }
 }
 
