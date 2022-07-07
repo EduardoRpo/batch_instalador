a4d7d9643fc9290101861edc94a0d94959bd5664
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-07-06 20:45:53 -0500

    calculo semana fecha de planeacion

--- html/js/batch/calcWeek.js
+++ html/js/batch/calcWeek.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     currentdate = new Date();
     var oneJan = new Date(currentdate.getFullYear(), 0, 1);
     var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-    var result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+    var result = Math.ceil((currentdate.getDay() + numberOfDays) / 7);
     $(`#numberWeek`).html(`Semana No. ${result}`);
 });
