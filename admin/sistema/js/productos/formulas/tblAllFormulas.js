1379db55db1c0939e2c9a4e0d0a4dbc926644f71
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-09-02 12:15:35 -0500

    ajuste todas las formulas archivo excel

--- admin/sistema/js/productos/formulas/tblAllFormulas.js
+++ admin/sistema/js/productos/formulas/tblAllFormulas.js
@@ -1,47 +1,46 @@
-$(document).ready(function () {
-  cargarTablaTodasFormulas = () => {
-    tabla = $('#tblFormulastodas').DataTable({
-      destroy: true,
-      scrollY: '50vh',
-      scrollCollapse: true,
-      paging: false,
-      oLanguage: { sProcessing: "<div id='loader'></div>" },
-      dom: 'Bfrtip',
-      order: [[0, 'asc']],
-      buttons: [
-        {
-          extend: 'excel',
-          className: 'btn btn-primary',
-          exportOptions: {
-            columns: [0, 1, 2, 4],
-          },
-        },
-      ],
-      language: { url: 'admin_componentes/es-ar.json' },
+$(document).ready(function() {
+    cargarTablaTodasFormulas = () => {
+        tabla = $('#tblFormulastodas').DataTable({
+            destroy: true,
+            scrollY: '50vh',
+            scrollCollapse: true,
+            paging: false,
+            oLanguage: { sProcessing: "<div id='loader'></div>" },
+            dom: 'Bfrtip',
+            order: [
+                [0, 'asc']
+            ],
+            buttons: [{
+                extend: 'excel',
+                className: 'btn btn-primary',
+                exportOptions: {
+                    columns: [0, 1, 2, 4],
+                },
+            }, ],
+            language: { url: 'admin_componentes/es-ar.json' },
 
-      ajax: {
-        url: '/api/formulatbl',
-        dataSrc: '',
-      },
+            ajax: {
+                url: '/api/formulatbl',
+                dataSrc: '',
+            },
 
-      columns: [
-        { title: 'Producto', data: 'id' },
-        { title: 'Referencia MP', data: 'referencia' },
-        { title: 'Materia prima', data: 'nombre' },
-        { title: 'Alias', data: 'alias' },
-        {
-          title: '%',
-          data: 'porcentaje',
-          className: 'centrado',
-          render: $.fn.dataTable.render.number(',', '.', 3, '', '%'),
-        },
-        {
-          title: 'Acciones',
-          defaultContent:
-            "<a href='#' <i class='large material-icons link-editar tr' data-toggle='tooltip' title='Actualizar' style='color:rgb(255, 165, 0)'>edit</i></a> <a href='#' <i class='large material-icons link-borrar tr' data-toggle='tooltip' title='Eliminar' style='color:rgb(255, 0, 0)'>clear</i></a>",
-        },
-      ],
-      columnDefs: [{ width: '10%', targets: 0 }],
-    });
-  };
+            columns: [
+                { title: 'Producto', data: 'id_producto' },
+                { title: 'Referencia MP', data: 'referencia' },
+                { title: 'Materia prima', data: 'nombre' },
+                { title: 'Alias', data: 'alias' },
+                {
+                    title: '%',
+                    data: 'porcentaje',
+                    className: 'centrado',
+                    render: $.fn.dataTable.render.number(',', '.', 3, '', '%'),
+                },
+                {
+                    title: 'Acciones',
+                    defaultContent: "<a href='#' <i class='large material-icons link-editar tr' data-toggle='tooltip' title='Actualizar' style='color:rgb(255, 165, 0)'>edit</i></a> <a href='#' <i class='large material-icons link-borrar tr' data-toggle='tooltip' title='Eliminar' style='color:rgb(255, 0, 0)'>clear</i></a>",
+                },
+            ],
+            columnDefs: [{ width: '10%', targets: 0 }],
+        });
+    };
 });
