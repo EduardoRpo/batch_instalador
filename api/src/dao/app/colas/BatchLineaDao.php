65882601e90e1213e82370b42efcf3abf3639ad1
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-11-03 18:37:15 -0500

    fix fecha_envasado interfaz programacion envasado

--- api/src/dao/app/colas/BatchLineaDao.php
+++ api/src/dao/app/colas/BatchLineaDao.php
@@ -73,7 +73,7 @@ class BatchLineaDao
   public function findBatchProgramacionEnvasado()
   {
     $connection = Connection::getInstance()->getConnection();
-    $stmt = $connection->prepare("SELECT DISTINCT batch.id_batch, date_add(batch.fecha_programacion, interval 3 day) AS fecha_programacion, DATE_ADD(batch.fecha_programacion, INTERVAL 13 DAY) AS fecha_envasado, batch.numero_orden, 
+    $stmt = $connection->prepare("SELECT DISTINCT batch.id_batch, date_add(batch.fecha_programacion, interval 3 day) AS fecha_programacion, DATE_ADD(batch.fecha_programacion, INTERVAL 3 DAY) AS fecha_envasado, batch.numero_orden, 
                                       batch.id_producto as referencia, p.nombre_referencia, batch.numero_lote, batch.unidad_lote, batch.tamano_lote, batch.estado, batch.multi, bcf.cantidad_firmas, bcf.total_firmas, batch.programacion_envasado, 
                                       (SELECT COUNT(*) FROM observaciones_batch_inactivos WHERE batch = batch.id_batch) AS cant_observations, batch.ok_aprobado
                                   FROM batch
