9ba03a8d16b50fe0c9c6505671fd8bf19a244cc2
Daniel Ledesma <daniel.ledesma@teenus.com.co>
2023-11-20 10:37:55 -0500

    change

--- html/pdf/js/download_pdf.js
+++ html/pdf/js/download_pdf.js
@@ -14,15 +14,17 @@ $(document).ready(function () {
       elementos = copy_invoice.getElementsByTagName('body');
       elementos[0].style.width = '1000px';
       elementos[0].style.margin = '-10px';
-      elementos[0].style.fontSize = 'small';
+      elementos[0].style.fontSize = 'small'; 
 
       // Obtén la URL base del servidor desde la ventana actual
       const baseUrl = window.location.origin;
  
       copy_invoice.querySelectorAll('img').forEach((img) => {
-        const originalSrc = img.getAttribute('src');
+        let originalSrc = img.getAttribute('src');
+        originalSrc = originalSrc.replace(/(\.\.\/)+/, '/');
         const newSrc = baseUrl + originalSrc;
         img.setAttribute('src', newSrc);
+        img.style.width = '150px';
       });
 
       let form = new FormData();
@@ -50,8 +52,8 @@ $(document).ready(function () {
       const a = document.createElement('a');
       a.style.display = 'none';
       a.href = url;
-      // a.download = `${data.numero_lote}-${data.id_batch}.pdf`;
-      a.download = `archivo.pdf`;
+      a.download = `${data.numero_lote}-${data.id_batch}.pdf`;
+      // a.download = `archivo.pdf`;
       document.body.appendChild(a);
     
       a.click();
