b8ebc42ec2e871fbbde552d215e52493b9ce849f
EduardoRpo <proyectosrestrepowp@gmail.com>
2025-03-03 20:21:49 -0500

    ajuste html despachos.php

--- html/pdf/modules/despachos.php
+++ html/pdf/modules/despachos.php
@@ -8,14 +8,28 @@
             </ul>
         </div>
     </div>
-    <div class="despachos">
-        <label for="">Entregó:</label><label for=""><b>JEFE DE PRODUCCIÓN</b></label>
-        <label for="">Recibió:</label><label for=""><b>AUX ALMACEN PRODUCTO TERMINADO</b></label>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <label for="">Entregó: </label><label for=""><b> PRODUCCIÓN</b></label>
+            </div>
+            <div class="col">
+            
+            </div>
+            <div class="col">
+            <label for="">Recibió: </label><label for=""><b> BODEGA PRODUCTO TERMINADO</b></label>
+            </div>
+        </div>
     </div>
-
+    <!--<div class="despachos">
+        <label for="">Entregó: </label><label for=""><b> PRODUCCIÓN</b></label>
+        <label for="">Recibió: </label><label for=""><b> BODEGA PRODUCTO TERMINADO</b></label>
+    </div> -->
+    <!--
     <div class="subtitle"><label for="">Observaciones</label></div>
     <div id="obs7" class="ml-5 mt-3 mb-3"></div>
-    <div class="subtitle"><label for=""></label></div>
+    <div class="subtitle"><label for=""></label></div>-->
 
     <!-- <div class="enlinea">
         <label class="mr-3" style="justify-self: end;">Fecha</label>
@@ -26,6 +40,7 @@
         <img id="f_entrego" src="" alt="firma_usuario" height="130">
         <label id="user_entrego"></label>
     </div> -->
+    <!--
     <table class="mt-3" id="firmas2" style="width:100%">
         <tbody>
             <tr>
@@ -42,5 +57,5 @@
                 <td class="text-center" id="user_entrego" colspan="3"></td>
             </tr>
         </tbody>
-    </table>
+    </table> -->
 </div>
