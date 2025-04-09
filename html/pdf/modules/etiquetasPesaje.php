d9009a83b17d80bb810eb1014a1aecca0b493fbd
EduardoRpo <proyectosrestrepowp@gmail.com>
2025-04-08 22:05:38 -0500

    encabezado anexos etiquetaspesaje

--- html/pdf/modules/etiquetasPesaje.php
+++ html/pdf/modules/etiquetasPesaje.php
@@ -13,42 +13,66 @@
     </div>
 
 <div class="card mt-5 mb-5">
-<div class="card-header centrado"><b>Anexo I - ORDEN DE PESAJE</b></div>
-<div class="card-body mb-5">
-    <div style="max-height: 400px;">
-        <table id="contenedorOrdenPesaje" style="width: 100%; border-collapse: collapse;" border="1">
-            <thead style="background-color: #FF7F50; color: white;">
-                <tr>
-                    <th style="padding: 10px;">Tamaño Lote</th>
-                    <th style="padding: 10px;">ID Producto</th>
-                    <th style="padding: 10px;">ID</th>
-                    <th style="padding: 10px;">Referencia</th>
-                    <th style="padding: 10px;">Alias</th>
-                    <th style="padding: 10px;">Porcentaje (%)</th>
-                    <th style="padding: 10px;">Peso (Kg)</th>
-                    <th style="padding: 10px;">Batch Lote Materiales</th>
-                </tr>
-                
-            </thead>
+    <div class="card-header centrado"><b>Anexo I - ORDEN DE PESAJE</b></div>
+    <table style="width:100%">
             <tbody>
-                <!-- Las filas se llenarán dinámicamente desde el backend -->
+                <tr>
+                    <td class="text-center product" colspan="11">CREMA CONTROL ESTRIAS - VIBRANTY (GRANEL ) </td>
+                </tr>
+                <tr style="white-space: nowrap;">
+    <td style="width: 1%;"></td>
+    <td style="width: 3%;"><b>FECHA:</b></td>
+    <td style="width: 10%;" class="fecha_pesaje">2023-06-22</td>
+    <td style="width: 7%;"><b>INVIMA:</b></td>
+    <td style="width: 13%;" class="invima_id">NSOC22530-23CO</td>
+    <td style="width: 7%;"><b>LINEA:</b></td>
+    <td style="width: 8%;" class="linea">SEMISÓLIDO</td>
+    <td style="width: 5%;"><b>LOTE:</b></td>
+    <td style="width: 10%;" class="lote_id">SM2310623</td>
+    <td style="width: 10%;"><b>TAMAÑO LOTE:</b></td>
+    <td class="tamanioLotePesaje" style="width: 10%;">110.16</td>
+    <td style="width: 10%; "><b>(%) FORMULA:</b></td>
+    <td style="width: 10%;"><input type="text" class="form-control" id="totalPorcentaje" readonly value="" style="border: none; background-color: transparent; box-shadow: none;"></td>
+</tr>
+
             </tbody>
         </table>
-    </div>
-     <!-- Fila con los totales al final de la tabla -->
-     <div style="margin-top: 20px; text-align: right;" class='mt-5'>
-        <label for="totalPorcentaje"><b>Total Porcentaje (%)</b></label>
-        <input type="text" class="form-control" id="totalPorcentaje" readonly value="" style="width: 200px; display: inline-block; margin-right: 10px;">
+    <div class="card-body mb-5">
+        <div style="max-height: 400px;">
+            <table id="contenedorOrdenPesaje" style="width: 100%; border-collapse: collapse;" border="1">
+                <thead style="background-color: #FF7F50; color: white;">
+                    <tr>
+                        <th style="padding: 10px;">Tamaño Lote</th>
+                        <th style="padding: 10px;">ID Producto</th>
+                        <th style="padding: 10px;">ID</th>
+                        <th style="padding: 10px;">Referencia</th>
+                        <th style="padding: 10px;">Alias</th>
+                        <th style="padding: 10px;">Porcentaje (%)</th>
+                        <th style="padding: 10px;">Peso (Kg)</th>
+                        <th style="padding: 10px;">Batch Lote Materiales</th>
+                    </tr>
+                    
+                </thead>
+                <tbody>
+                    <!-- Las filas se llenarán dinámicamente desde el backend -->
+                </tbody>
+            </table>
+        </div>
+       
+        <!-- Fila con los totales al final de la tabla -->
+        <div style="margin-top: 5rem; text-align: right;" ></div>
+        <!--
+            <label for="totalPorcentaje"><b>Total Porcentaje (%)</b></label>
+            <input type="text" class="form-control" id="totalPorcentaje" readonly value="" style="width: 200px; display: inline-block; margin-right: 10px;">
 
-        <label for="pesoTotalPeso"><b>Total Peso (Kg)</b></label>
-        <input type="text" class="form-control" id="pesoTotalPeso" readonly value="" style="width: 200px; display: inline-block;">
+            <label for="pesoTotalPeso"><b>Total Peso (Kg)</b></label>
+            <input type="text" class="form-control" id="pesoTotalPeso" readonly value="" style="width: 200px; display: inline-block;">
+        </div>-->
     </div>
-</div>
 
     
-    
 
-    <div class="card-header centrado"><b>ETIQUETAS PROCESO PESAJE</b></div>
+    <div class="card-header centrado mt-5"><b>ETIQUETAS PROCESO PESAJE F</b></div>
     <div class="card-body">
 
         <div id="contenedorEtiquetasV" class="contenedorEtiquetasV">
