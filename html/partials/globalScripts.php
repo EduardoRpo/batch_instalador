d7e7e0bc59bd18609a593d53d025c916516049ed
EduardoRpo <proyectosrestrepowp@gmail.com>
2025-09-01 22:59:02 -0500

    Revert "add: marcar automaticamente boton de verifico en despeje"
    This reverts commit c8f1588338081f800c74c41fed44e810c1b0f3df.

--- html/partials/globalScripts.php
+++ html/partials/globalScripts.php
@@ -14,4 +14,3 @@
 <script src="/html/js/firmar/firmar1raSeccion.js"></script>
 <script src="/html/js/firmar/firmar2daSeccion.js"></script>
 <script src="/html/js/global/searchData.js"></script>
-<script src="/html/js/global/verificacionPesaje.js"></script>
